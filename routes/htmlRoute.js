4b9c971672a340d9087c9f6c46a44a160cde403a
mhmunter <mark.munter1@gmail.com>
2022-04-24 17:16:14 -0500

    added to routes

--- routes/htmlRoute.js
+++ routes/htmlRoute.js
@@ -4,11 +4,11 @@ const path = require("path");
 // const app = express();
 const router = require('express').Router();
 
-router.get('/notes', (req, res) => {
+api.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, 'notes.html'));
   })
 
-  router.get('*', (req, res) => {
+  app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
   })
 
